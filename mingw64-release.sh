424b1493c4b4b4bc728aa133698cf149a91508f7
kth5 <kthfive@gmail.com>
2018-05-18 15:41:17 +0800

    also attempt the same dll finding for the dlls copied in

--- mingw64-release.sh
+++ mingw64-release.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+rm -rf bin
+mkdir -p bin
+
 ./mingw64aes.sh
 ./mingw64avx+aes.sh
 ./mingw64avx2+aes.sh
 
+# attempt to find our sys-root
 BIN_PATH=`dirname $(find /usr/x86_64-w64-mingw32/ -name zlib1.dll)`
-BIN_DLLS=`strings bin/hodlminer_AES.exe  | grep '\.dll' | grep -Ev "KERNEL32.dll|USER32.dll|WS2_32.dll"`
 
-mkdir -p bin
+# find out dependencies / some may not exist but that's ok. 
+BIN_DLLS=`strings bin/hodlminer_AES.exe  | grep '\.dll'`
+
 for dll in ${BIN_DLLS}; do
-	cp -v ${BIN_PATH}/${dll} bin/
+	cp ${BIN_PATH}/${dll} bin/
+done
+
+# same all over again for the dlls
+BIN_DLLS=`strings bin/*.dll  | grep '\.dll'`
+
+for dll in ${BIN_DLLS}; do
+        cp ${BIN_PATH}/${dll} bin/
 done
